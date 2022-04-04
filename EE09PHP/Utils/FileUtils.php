de6ea34a6a48107377193b5152892596cd1eb4da
david <d.marsalone@gmail.com>
2022-04-04 08:23:11 +0200

    Update FileUtils.php

--- EE09PHP/Utils/FileUtils.php
+++ EE09PHP/Utils/FileUtils.php
@@ -1,128 +1,209 @@
 <?php
+namespace EE09PHP\Utils;
 
-$vv=new \EE09\ApiResponse();
-the()->apiResponse=$vv;
-$vv->usePayload();
+use FilesystemIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 
-$vv->addMessage("mise à jour d'une instances");
-
-// test des variables request
-//TODO captcha ou un truc pour éviter les attaques DOS
-
-
-// l'instance à mettre à jour
-$name=$vv->testAndGetRequest("name");
-$inst=null;
-if($vv->success){
-    $inst=new \Models\RecapInstance($name);
-    if(!$inst->isValid){
-        //$vv->addError("instance $name invalide");
+/**
+ * Quelques méthodes pratiques pour gérer des fichiers
+ * @package EE09\Core\Utils
+ * @method static FileUtils inst()
+ */
+class FileUtils extends AbstractSingleton
+{
+    /**
+     * Renvoie l'extension du fichier donné
+     * @param string $filename
+     * @return string
+     */
+    public function extension($filename,$dot=false){
+        $prefix="";
+        if($dot){
+            $prefix=".";
+        }
+        $ext=pathinfo($filename, PATHINFO_EXTENSION);
+        return $prefix.$ext;
     }
-    $vv->addToBody("inst",$inst);
+    /**
+     * Convertit des octets en un truc lisible par un humain
+     * @param int $bytes The file size in bytes
+     * @param bool $decimals si true affichera les décimales
+     * @return string Something like 103 MB
+     */
+    public function humanFileSize($bytes,$decimals=false)
+    {
 
+        if($decimals){
+            $decimals=2;
+        }else{
+            $decimals=0;
+        }
+        if ($bytes >= 1073741824){
+            $bytes = number_format($bytes / 1073741824, $decimals) . ' GB';
+        }
+        elseif ($bytes >= 1048576){
+            $bytes = number_format($bytes / 1048576, $decimals) . ' MB';
+        }
+        elseif ($bytes >= 1024){
+            $bytes = number_format($bytes / 1024, $decimals) . ' KB';
+        }
+        elseif ($bytes > 1){
+            $bytes = $bytes . ' bytes';
+        }
+        elseif ($bytes == 1){
+            $bytes = $bytes . ' byte';
+        }
+        else{
+            $bytes = '0 bytes';
+        }
+
+        return $bytes;
+
+    }
+
+    /**
+     * A partir d'un mime type renvoie une extension de fichier (sans le point)
+     * @param string $mime
+     * @return int|string
+     */
+    public function mimeToExtension($mime){
+        $all_mimes = '{"png":["image\/png","image\/x-png"],"bmp":["image\/bmp","image\/x-bmp","image\/x-bitmap","image\/x-xbitmap","image\/x-win-bitmap","image\/x-windows-bmp","image\/ms-bmp","image\/x-ms-bmp","application\/bmp","application\/x-bmp","application\/x-win-bitmap"],"gif":["image\/gif"],"jpeg":["image\/jpeg","image\/pjpeg"],"xspf":["application\/xspf+xml"],"vlc":["application\/videolan"],"wmv":["video\/x-ms-wmv","video\/x-ms-asf"],"au":["audio\/x-au"],"ac3":["audio\/ac3"],"flac":["audio\/x-flac"],"ogg":["audio\/ogg","video\/ogg","application\/ogg"],"kmz":["application\/vnd.google-earth.kmz"],"kml":["application\/vnd.google-earth.kml+xml"],"rtx":["text\/richtext"],"rtf":["text\/rtf"],"jar":["application\/java-archive","application\/x-java-application","application\/x-jar"],"zip":["application\/x-zip","application\/zip","application\/x-zip-compressed","application\/s-compressed","multipart\/x-zip"],"7zip":["application\/x-compressed"],"xml":["application\/xml","text\/xml"],"svg":["image\/svg+xml"],"3g2":["video\/3gpp2"],"3gp":["video\/3gp","video\/3gpp"],"mp4":["video\/mp4"],"m4a":["audio\/x-m4a"],"f4v":["video\/x-f4v"],"flv":["video\/x-flv"],"webm":["video\/webm"],"aac":["audio\/x-acc"],"m4u":["application\/vnd.mpegurl"],"pdf":["application\/pdf","application\/octet-stream"],"pptx":["application\/vnd.openxmlformats-officedocument.presentationml.presentation"],"ppt":["application\/powerpoint","application\/vnd.ms-powerpoint","application\/vnd.ms-office","application\/msword"],"docx":["application\/vnd.openxmlformats-officedocument.wordprocessingml.document"],"xlsx":["application\/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application\/vnd.ms-excel"],"xl":["application\/excel"],"xls":["application\/msexcel","application\/x-msexcel","application\/x-ms-excel","application\/x-excel","application\/x-dos_ms_excel","application\/xls","application\/x-xls"],"xsl":["text\/xsl"],"mpeg":["video\/mpeg"],"mov":["video\/quicktime"],"avi":["video\/x-msvideo","video\/msvideo","video\/avi","application\/x-troff-msvideo"],"movie":["video\/x-sgi-movie"],"log":["text\/x-log"],"txt":["text\/plain"],"css":["text\/css"],"html":["text\/html"],"wav":["audio\/x-wav","audio\/wave","audio\/wav"],"xhtml":["application\/xhtml+xml"],"tar":["application\/x-tar"],"tgz":["application\/x-gzip-compressed"],"psd":["application\/x-photoshop","image\/vnd.adobe.photoshop"],"exe":["application\/x-msdownload"],"js":["application\/x-javascript"],"mp3":["audio\/mpeg","audio\/mpg","audio\/mpeg3","audio\/mp3"],"rar":["application\/x-rar","application\/rar","application\/x-rar-compressed"],"gzip":["application\/x-gzip"],"hqx":["application\/mac-binhex40","application\/mac-binhex","application\/x-binhex40","application\/x-mac-binhex40"],"cpt":["application\/mac-compactpro"],"bin":["application\/macbinary","application\/mac-binary","application\/x-binary","application\/x-macbinary"],"oda":["application\/oda"],"ai":["application\/postscript"],"smil":["application\/smil"],"mif":["application\/vnd.mif"],"wbxml":["application\/wbxml"],"wmlc":["application\/wmlc"],"dcr":["application\/x-director"],"dvi":["application\/x-dvi"],"gtar":["application\/x-gtar"],"php":["application\/x-httpd-php","application\/php","application\/x-php","text\/php","text\/x-php","application\/x-httpd-php-source"],"swf":["application\/x-shockwave-flash"],"sit":["application\/x-stuffit"],"z":["application\/x-compress"],"mid":["audio\/midi"],"aif":["audio\/x-aiff","audio\/aiff"],"ram":["audio\/x-pn-realaudio"],"rpm":["audio\/x-pn-realaudio-plugin"],"ra":["audio\/x-realaudio"],"rv":["video\/vnd.rn-realvideo"],"jp2":["image\/jp2","video\/mj2","image\/jpx","image\/jpm"],"tiff":["image\/tiff"],"eml":["message\/rfc822"],"pem":["application\/x-x509-user-cert","application\/x-pem-file"],"p10":["application\/x-pkcs10","application\/pkcs10"],"p12":["application\/x-pkcs12"],"p7a":["application\/x-pkcs7-signature"],"p7c":["application\/pkcs7-mime","application\/x-pkcs7-mime"],"p7r":["application\/x-pkcs7-certreqresp"],"p7s":["application\/pkcs7-signature"],"crt":["application\/x-x509-ca-cert","application\/pkix-cert"],"crl":["application\/pkix-crl","application\/pkcs-crl"],"pgp":["application\/pgp"],"gpg":["application\/gpg-keys"],"rsa":["application\/x-pkcs7"],"ics":["text\/calendar"],"zsh":["text\/x-scriptzsh"],"cdr":["application\/cdr","application\/coreldraw","application\/x-cdr","application\/x-coreldraw","image\/cdr","image\/x-cdr","zz-application\/zz-winassoc-cdr"],"wma":["audio\/x-ms-wma"],"vcf":["text\/x-vcard"],"srt":["text\/srt"],"vtt":["text\/vtt"],"ico":["image\/x-icon","image\/x-ico","image\/vnd.microsoft.icon"],"csv":["text\/x-comma-separated-values","text\/comma-separated-values","application\/vnd.msexcel"],"json":["application\/json","text\/json"]}';
+        $all_mimes = json_decode($all_mimes,true);
+        foreach ($all_mimes as $key => $value) {
+            if(array_search($mime,$value) !== false) return $key;
+        }
+        return "";
+    }
+
+    /**
+     * Copie un dossier de manière récursive
+     * @param $src
+     * @param $dest
+     * @return boolean
+     */
+    public function recurseCopy( $sourceDirectory, $destinationDirectory, $childFolder = '') {
+        $directory = opendir($sourceDirectory);
+
+        if (is_dir($destinationDirectory) === false) {
+            $success=mkdir($destinationDirectory);
+            if(!$success){
+                return false;
+            }
+        }
+
+        if ($childFolder !== '') {
+            if (is_dir("$destinationDirectory/$childFolder") === false) {
+                $success=mkdir("$destinationDirectory/$childFolder");
+                if(!$success){
+                    return false;
+                }
+            }
+
+            while (($file = readdir($directory)) !== false) {
+                if ($file === '.' || $file === '..') {
+                    continue;
+                }
+                if (is_dir("$sourceDirectory/$file") === true) {
+                    $success=$this->recurseCopy("$sourceDirectory/$file", "$destinationDirectory/$childFolder/$file");
+                    if(!$success){
+                        return false;
+                    }
+                } else {
+                    $success=copy("$sourceDirectory/$file", "$destinationDirectory/$childFolder/$file");
+                    if(!$success){
+                        return false;
+                    }
+                }
+            }
+            closedir($directory);
+            return true;
+        }
+
+        while (($file = readdir($directory)) !== false) {
+            if ($file === '.' || $file === '..') {
+                continue;
+            }
+
+            if (is_dir("$sourceDirectory/$file") === true) {
+                $success=$this->recurseCopy("$sourceDirectory/$file", "$destinationDirectory/$file");
+                if(!$success){
+                    return false;
+                }
+            }
+            else {
+                $success=copy("$sourceDirectory/$file", "$destinationDirectory/$file");
+                if(!$success){
+                    return false;
+                }
+            }
+        }
+
+        closedir($directory);
+        return true;
+    }
+
+    /**
+     * @param $sourceDirectory
+     * @param $destinationDirectory
+     * @param $childFolder
+     * @return false|void
+     */
+    public function copyDir($sourceDirectory, $destinationDirectory, $childFolder = ''){
+        return $this->recurseCopy($sourceDirectory, $destinationDirectory, $childFolder);
+    }
+    /**
+     * Supprime un répertoire et tout ce qui est dedans
+     * @param $dirPath
+     * @return boolean|null renvoie null si le répertoire n'existe pas, true si il a été supprimé, false si une erreur s'est produite
+     */
+    public function removeDir($dirPath) {
+        if (is_dir($dirPath)) {
+            $objects = scandir($dirPath);
+            foreach ($objects as $object) {
+                if ($object != "." && $object !="..") {
+                    if (filetype($dirPath . DIRECTORY_SEPARATOR . $object) == "dir") {
+                        $success=$this->removeDir($dirPath . DIRECTORY_SEPARATOR . $object);
+                        if($success === false){
+                            return false;
+                        }
+                    } else {
+                        unlink($dirPath . DIRECTORY_SEPARATOR . $object);
+                    }
+                }
+            }
+            reset($objects);
+            return rmdir($dirPath);;
+        }
+        return null;
+
+
+
+    }
+    /**
+     * Crée le répertoire (récursivement) d'un fichier donné
+     * @param string $filePath Chemin vers le fichier
+     */
+    public function prepareDir($filePath){
+        $dir=dirname($filePath);
+        if(!is_dir($dir)){
+            mkdir($dir,0777,true);
+        }
+    }
+
+    /**
+     * Revoie la taille du répertoire en récursif
+     * @param string $path
+     * @return int
+     */
+    public function dirSize($path){
+        $bytestotal = 0;
+        $path = realpath($path);
+        if($path!==false && $path!='' && file_exists($path)){
+            foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS)) as $object){
+                $bytestotal += $object->getSize();
+            }
+        }
+        return $bytestotal;
+    }
 }
-
-// l'instance master
-$master=new \Models\RecapInstance("master");
-if($vv->success){
-    if(!$master->isValid){
-        $vv->addError("Master invalide");
-    }else{
-        $vv->addToBody("master",$master);
-    }
-}
-
-if($vv->success){
-    $vv->addMessage("passage de la version $inst->version à la version $master->version");
-
-    //répertoires à remplacer
-    $replaceDirs=[
-        "admin",
-        "js",
-        "fonts",
-        "css",
-        "img",
-        "server/vendor",
-        "server/project",
-        "server/php-classes",
-        "server/configs",
-    ];
-
-    //fichiers à remplacer
-    $replaceFiles=[
-        "confs/default-texts.json",
-        ".htaccess",
-        "favicon.ico",
-        "index.html",
-        "service-worker.js",
-        "manifest.json",
-        "version.txt",
-    ];
-
-    $vv->addMessage("Répertoires...");
-    foreach ($replaceDirs as $item){
-        $path="../".$inst->name."/".$item;
-        $masterPath="../master/".$item;
-        //effaçage de répertoires
-        if(is_dir($path)){
-            $success=utils()->files->removeDir($path);
-            $vv->addMessage("Effacer $path",$success);
-            if($success===false){
-                $vv->addError("Effacer dir $path failed");
-            }
-        }else{
-            $vv->addMessage("instance $path introuvable");
-        }
-
-
-        //copie de répertoires
-        if(is_dir($masterPath)){
-            $vv->addMessage("Remplacer par $masterPath");
-            $success=utils()->files->copyDir($masterPath,$path);
-            if($success===false){
-                $vv->addError("Copie de dir $path failed");
-            }
-        }else{
-            $vv->addError("master dir $masterPath invalide");
-        }
-
-    }
-
-
-    $vv->addMessage("Fichiers.............");
-    foreach ($replaceFiles as $item){
-        $path="../".$inst->name."/".$item;
-        $masterPath="../master/".$item;
-        //effaçage de fichiers
-        if(is_file($path)){
-            $success=unlink($path);
-            $vv->addMessage("Effacer $path",$success);
-            if($success===false){
-                $vv->addError("Effacer file $path failed");
-            }
-        }else{
-            $vv->addMessage("instance $path introuvable");
-        }
-
-        //copie de fichiers
-        if(is_file($masterPath)){
-            $vv->addMessage("Remplacer par $masterPath");
-            $success=copy($masterPath,$path);
-            if($success===false){
-                $vv->addError("Copie de file $path failed");
-            }
-        }else{
-            $vv->addError("master file $masterPath invalide");
-        }
-
-    }
-
-
-
-    //TODO gérer precache-manifest-xxxxx.js
-
-
-}
-
-
-$view->inside("json", $vv);
